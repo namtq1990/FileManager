ebb6c271d53187ae1ab72de178830794231aad34
namtq1990 <mrquangtn@gmail.com>
2014-11-10 15:55:02 +0700

    + Add background, file icon
    Separate between file and folder icon

--- src/com/example/filemanager/FileListAdapter.java
+++ src/com/example/filemanager/FileListAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -35,10 +36,14 @@ public class FileListAdapter extends ArrayAdapter<File>{
 		File item = getItem(position);
 		if (item != null) {
 			TextView tv = (TextView)newView.findViewById(R.id.title_item);
-			//ImageButton button = (ImageButton)newView.findViewById(R.id.icon_item);
-			//button.setFocusable(false);
-			//button.setClickable(false);
 			tv.setText(item.getName());
+			
+			ImageView icon = (ImageView)newView.findViewById(R.id.icon_item);
+
+			if (item.isDirectory()) {
+				icon.setImageResource(R.drawable.folder_icon);
+			}
+			else icon.setImageResource(R.drawable.file_icon);
 		}
 		
 		return newView;
